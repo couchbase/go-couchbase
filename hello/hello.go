5a9e0eb231f79aa9dc08dc7a1158d86dd597a8bd
Dustin Sallings <dustin@spy.net>
2012-01-19 14:56:32 -0800

    Split apart the example a bit to make it easier to grok.

--- hello/hello.go
+++ hello/hello.go
@@ -28,6 +28,37 @@ func doOps(b couchbase.Bucket) {
 	}
 }
 
+func exploreBucket(bucket couchbase.Bucket) {
+	fmt.Printf("     %v uses %s\n", bucket.Name,
+		bucket.VBucketServerMap.HashAlgorithm)
+	for pos, server := range bucket.VBucketServerMap.ServerList {
+		vbs := make([]string, 0, 1024)
+		for vb, a := range bucket.VBucketServerMap.VBucketMap {
+			if a[0] == pos {
+				vbs = append(vbs, strconv.Itoa(vb))
+			}
+		}
+		fmt.Printf("        %s: %v\n", server, vbs)
+	}
+
+	doOps(bucket)
+
+}
+
+func explorePool(pool couchbase.Pool) {
+	for _, n := range pool.Nodes {
+		fmt.Printf("     %v\n", n.Hostname)
+	}
+	fmt.Printf("  Buckets:\n")
+	for n, _ := range pool.Buckets {
+		bucket, err := pool.GetBucket(n)
+		if err != nil {
+			log.Fatalf("Error getting bucket:  %v\n", err)
+		}
+		exploreBucket(bucket)
+	}
+}
+
 func main() {
 	flag.Parse()
 	c, err := couchbase.Connect(flag.Arg(0))
@@ -41,28 +72,6 @@ func main() {
 		if err != nil {
 			log.Fatalf("Can't get pool:  %v", err)
 		}
-		for _, n := range p.Nodes {
-			fmt.Printf("     %v\n", n.Hostname)
-		}
-		fmt.Printf("  Buckets:\n")
-		for n, _ := range p.Buckets {
-			bucket, err := p.GetBucket(n)
-			if err != nil {
-				log.Fatalf("Error getting bucket:  %v\n", err)
-			}
-			fmt.Printf("     %v uses %s\n", bucket.Name,
-				bucket.VBucketServerMap.HashAlgorithm)
-			for pos, server := range bucket.VBucketServerMap.ServerList {
-				vbs := make([]string, 0, 1024)
-				for vb, a := range bucket.VBucketServerMap.VBucketMap {
-					if a[0] == pos {
-						vbs = append(vbs, strconv.Itoa(vb))
-					}
-				}
-				fmt.Printf("        %s: %v\n", server, vbs)
-			}
-
-			doOps(bucket)
-		}
+		explorePool(p)
 	}
 }
