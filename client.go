c16c9463ec647aa137fc4f0508d6ede1b1a1d7b4
Dustin Sallings <dustin@spy.net>
2012-01-19 15:33:19 -0800

    Simplify the client a bit.

--- client.go
+++ client.go
@@ -4,51 +4,48 @@ import (
 	"encoding/json"
 )
 
-func (b *Bucket) do(k string, f func(mc *memcachedClient, vb uint16) error) error {
+func (b *Bucket) getVBInfo(k string) (*memcachedClient, uint16) {
 	vb := b.VBHash(k)
 	masterId := b.VBucketServerMap.VBucketMap[vb][0]
 	if b.connections[masterId] == nil {
 		b.connections[masterId] = connect("tcp", b.VBucketServerMap.ServerList[masterId])
 	}
-	return f(b.connections[masterId], uint16(vb))
+	return b.connections[masterId], uint16(vb)
 }
 
 // Set a value in this bucket.
 // The value will be serialized into a JSON document.
 func (b *Bucket) Set(k string, v interface{}) error {
-	return b.do(k, func(mc *memcachedClient, vb uint16) error {
-		data, err := json.Marshal(v)
-		if err != nil {
-			return err
-		}
-		res := mc.Set(vb, k, 0, 0, data)
-		if res.Status != mcSUCCESS {
-			return res
-		}
-		return nil
-	})
+	mc, vb := b.getVBInfo(k)
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	res := mc.Set(vb, k, 0, 0, data)
+	if res.Status != mcSUCCESS {
+		return res
+	}
+	return nil
 }
 
 // Get a value from this bucket.
 // The value is expected to be a JSON stream and will be deserialized
 // into rv.
 func (b *Bucket) Get(k string, rv interface{}) error {
-	return b.do(k, func(mc *memcachedClient, vb uint16) error {
-		res := mc.Get(vb, k)
-		if res.Status != mcSUCCESS {
-			return res
-		}
-		return json.Unmarshal(res.Body, rv)
-	})
+	mc, vb := b.getVBInfo(k)
+	res := mc.Get(vb, k)
+	if res.Status != mcSUCCESS {
+		return res
+	}
+	return json.Unmarshal(res.Body, rv)
 }
 
 // Delete a key from this bucket.
 func (b *Bucket) Delete(k string) error {
-	return b.do(k, func(mc *memcachedClient, vb uint16) error {
-		res := mc.Del(vb, k)
-		if res.Status != mcSUCCESS {
-			return res
-		}
-		return nil
-	})
+	mc, vb := b.getVBInfo(k)
+	res := mc.Del(vb, k)
+	if res.Status != mcSUCCESS {
+		return res
+	}
+	return nil
 }
