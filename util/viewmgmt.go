3f0716ce9b4ea23a7d65bd936014267ae5b4755d
Dustin Sallings <dustin@spy.net>
2013-06-12 19:41:24 -0700

    Added view management in a utility library.

---
+++ util/viewmgmt.go
@@ -0,0 +1,49 @@
+// Utilities for working with Couchbase
+package couchbaseutil
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/couchbaselabs/go-couchbase"
+)
+
+// A marker stored in your DB to mark a particular view version.
+type ViewMarker struct {
+	Version   int       `json:"version"`
+	Timestamp time.Time `json:"timestamp"`
+	Type      string    `json:"type"`
+}
+
+// Install or update a view.
+//
+// This creates a document that tracks the version of design document
+// in couchbase and updates it if it's behind the version specified.
+//
+// A ViewMarker is stored with a type of "viewmarker" under the key
+// specified by `markerKey` to keep up with the view info.
+func UpdateView(d *couchbase.Bucket,
+	ddocName, markerKey, ddocBody string, version int) error {
+
+	marker := ViewMarker{}
+	err := d.Get(markerKey, &marker)
+	if err != nil {
+		log.Printf("Error checking view version: %v", err)
+	}
+	if marker.Version < version {
+		log.Printf("Installing new version of views (old version=%v)",
+			marker.Version)
+		doc := json.RawMessage([]byte(ddocBody))
+		err = d.PutDDoc(ddocName, &doc)
+		if err != nil {
+			return err
+		}
+		marker.Version = version
+		marker.Timestamp = time.Now().UTC()
+		marker.Type = "viewmarker"
+
+		return d.Set(markerKey, 0, &marker)
+	}
+	return nil
+}
