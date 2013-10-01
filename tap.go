a230b9df000c1d9704fbe29feb1da8c9be4694d4
Dustin Sallings <dustin@spy.net>
2013-10-01 10:20:40 -0700

    Automatically reconnect to tap on failure
    Jens Alfke wrote about half of this and it looked pretty good.  I did
    the parts that close.
    
    I tested under a few error conditions as well as successes (both
    continuing after failure and an explicit shutdown and verifying the
    goroutines cleaned up properly and stuff).

--- tap.go
+++ tap.go
@@ -1,18 +1,25 @@
 package couchbase
 
 import (
-	"sync"
+	"log"
+	"time"
 
 	"github.com/dustin/gomemcached/client"
 )
 
+const kInitialRetryInterval = 1 * time.Second
+const kMaximumRetryInterval = 30 * time.Second
+
 // A Tap feed. Events from the bucket can be read from the channel 'C'.
 // Remember to call Close() on it when you're done, unless its channel has closed itself already.
 type TapFeed struct {
-	C       <-chan memcached.TapEvent
-	output  chan memcached.TapEvent
-	feeds   []*memcached.TapFeed
-	waiters sync.WaitGroup
+	C <-chan memcached.TapEvent
+
+	bucket    *Bucket
+	args      *memcached.TapArguments
+	nodeFeeds []*memcached.TapFeed    // The TAP feeds of the individual nodes
+	output    chan memcached.TapEvent // Same as C but writeably-typed
+	quit      chan bool
 }
 
 // Creates and starts a new Tap feed
@@ -22,47 +29,104 @@ func (b *Bucket) StartTapFeed(args *memcached.TapArguments) (*TapFeed, error) {
 		args = &defaultArgs
 	}
 
-	feed := &TapFeed{output: make(chan memcached.TapEvent, 10)}
-	for _, serverConn := range b.connections {
-		singleFeed, err := serverConn.StartTapFeed(args)
-		if err != nil {
-			feed.Close()
-			return nil, err
-		}
-		feed.feeds = append(feed.feeds, singleFeed)
-		feed.waiters.Add(1)
-		go feed.forwardTapEvents(singleFeed.C)
+	feed := &TapFeed{
+		bucket: b,
+		args:   args,
+		output: make(chan memcached.TapEvent, 10),
+		quit:   make(chan bool),
 	}
-	go feed.closeWhenDone()
+
+	go feed.run()
 
 	feed.C = feed.output
 	return feed, nil
-
 }
 
-// Goroutine that forwards Tap events from a single node's feed to the aggregate feed.
-func (feed *TapFeed) forwardTapEvents(singleFeed <-chan memcached.TapEvent) {
-	defer feed.waiters.Done()
+// Goroutine that runs the feed
+func (feed *TapFeed) run() {
+	retryInterval := kInitialRetryInterval
+	bucketOK := true
 	for {
-		if event, ok := <-singleFeed; ok {
-			feed.output <- event
-		} else {
-			break
+		// Connect to the TAP feed of each server node:
+		if bucketOK {
+			killSwitch, err := feed.connectToNodes()
+			if err == nil {
+				// Run until one of the sub-feeds fails:
+				select {
+				case <-killSwitch:
+				case <-feed.quit:
+					return
+				}
+				feed.closeNodeFeeds()
+				retryInterval = kInitialRetryInterval
+			}
+		}
+
+		// On error, try to refresh the bucket in case the list of nodes changed:
+		log.Printf("go-couchbase: TAP connection lost; reconnecting to bucket %q in %v",
+			feed.bucket.Name, retryInterval)
+		err := feed.bucket.refresh()
+		bucketOK = err == nil
+
+		select {
+		case <-time.After(retryInterval):
+		case <-feed.quit:
+			return
+		}
+		if retryInterval *= 2; retryInterval > kMaximumRetryInterval {
+			retryInterval = kMaximumRetryInterval
 		}
 	}
 }
 
-// Goroutine that closes the bucket's tap feed channel when all input feeds have closed
-func (feed *TapFeed) closeWhenDone() {
-	feed.waiters.Wait()
-	close(feed.output)
+func (feed *TapFeed) connectToNodes() (killSwitch chan bool, err error) {
+	killSwitch = make(chan bool)
+	for _, serverConn := range feed.bucket.connections {
+		var singleFeed *memcached.TapFeed
+		singleFeed, err = serverConn.StartTapFeed(feed.args)
+		if err != nil {
+			feed.closeNodeFeeds()
+			return
+		}
+		feed.nodeFeeds = append(feed.nodeFeeds, singleFeed)
+		go feed.forwardTapEvents(singleFeed, killSwitch)
+	}
+	return
+}
+
+// Goroutine that forwards Tap events from a single node's feed to the aggregate feed.
+func (feed *TapFeed) forwardTapEvents(singleFeed *memcached.TapFeed, killSwitch chan bool) {
+	for {
+		select {
+		case event, ok := <-singleFeed.C:
+			if !ok {
+				killSwitch <- true
+				return
+			}
+			feed.output <- event
+		case <-feed.quit:
+			return
+		}
+	}
+}
+
+func (feed *TapFeed) closeNodeFeeds() {
+	for _, f := range feed.nodeFeeds {
+		f.Close()
+	}
+	feed.nodeFeeds = nil
 }
 
 // Closes a Tap feed.
-func (feed *TapFeed) Close() {
-	for _, f := range feed.feeds {
-		f.Close()
+func (feed *TapFeed) Close() error {
+	select {
+	case <-feed.quit:
+		return nil
+	default:
 	}
-	feed.feeds = nil
-	feed.C = nil
+
+	feed.closeNodeFeeds()
+	close(feed.quit)
+	close(feed.output)
+	return nil
 }
