cbcfdb644f3f7cb2bda480045876eef857cbc61f
Dustin Sallings <dustin@spy.net>
2013-10-29 00:47:47 -0700

    test URL parser

--- util_test.go
+++ util_test.go
@@ -18,3 +18,26 @@ func TestFindCommonSuffix(t *testing.T) {
 	assert(t, "one", FindCommonSuffix([]string{"blah"}), "")
 	assert(t, "two", FindCommonSuffix([]string{"blah.com", "foo.com"}), ".com")
 }
+
+func TestParseURL(t *testing.T) {
+	tests := []struct {
+		in    string
+		works bool
+	}{
+		{"", false},
+		{"http://whatever/", true},
+		{"http://%/", false},
+	}
+
+	for _, test := range tests {
+		got, err := ParseURL(test.in)
+		switch {
+		case err == nil && test.works,
+			!(err == nil || test.works):
+		case err == nil && !test.works:
+			t.Errorf("Expected failure on %v, got %v", test.in, got)
+		case test.works && err != nil:
+			t.Errorf("Expected success on %v, got %v", test.in, err)
+		}
+	}
+}
