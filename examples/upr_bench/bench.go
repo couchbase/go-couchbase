65dee3ebb214958b40ce47a6a5ba71dd15b16733
prataprc <prataprc@gmail.com>
2014-02-03 13:22:38 +0530

    Examples to test/use UPR sdk.

---
+++ examples/upr_bench/bench.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prataprc/go-couchbase"
+	"log"
+	"time"
+)
+
+var vbcount = 8
+
+const TESTURL = "http://localhost:9000"
+
+// Flush the bucket before trying this program
+func main() {
+	// get a bucket and mc.Client connection
+	bucket, err := getTestConnection("default")
+	if err != nil {
+		panic(err)
+	}
+
+	// start upr feed
+	name := fmt.Sprintf("%v", time.Now().UnixNano())
+	feed, err := couchbase.StartUprFeed(bucket, name, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// add mutations to the bucket.
+	var mutationCount = int64(100000)
+	go addKVset(bucket, int(mutationCount))
+
+	// observe the mutations from the channel.
+	mutations := int64(0)
+	start := time.Now().UnixNano()
+	end := start
+	for {
+		e := <-feed.C
+		log.Println(e.Opstr, e.Seqno, "helo world", "bucket", string(e.Key), string(e.Value))
+		mutations += 1
+		if mutationCount == mutations {
+			break
+		}
+		if mutations%(mutationCount/5) == 0 {
+			end = time.Now().UnixNano()
+			log.Printf("Recieved %v mutations %v time lapse per mutation\n",
+				mutations, (end-start)/mutations)
+		}
+	}
+	feed.Close()
+}
+
+func getTestConnection(bucketname string) (*couchbase.Bucket, error) {
+	couch, err := couchbase.Connect(TESTURL)
+	if err != nil {
+		log.Println("Make sure that couchbase is at", TESTURL)
+		return nil, err
+	}
+	pool, err := couch.GetPool("default")
+	if err != nil {
+		return nil, err
+	}
+	bucket, err := pool.GetBucket(bucketname)
+	return bucket, err
+}
+
+func addKVset(b *couchbase.Bucket, count int) {
+	for i := 0; i < count; i++ {
+		key := fmt.Sprintf("key%v", i)
+		value := fmt.Sprintf("Hello world%v", i)
+		err := b.Set(key, 0, value)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ examples/upr_feed/feed.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prataprc/go-couchbase"
+	"log"
+	"time"
+)
+
+var vbcount = 8
+
+const TESTURL = "http://localhost:9000"
+
+// Flush the bucket before trying this program
+func main() {
+	// get a bucket and mc.Client connection
+	bucket, err := getTestConnection("default")
+	if err != nil {
+		panic(err)
+	}
+
+	// start upr feed
+	name := fmt.Sprintf("%v", time.Now().UnixNano())
+	feed, err := couchbase.StartUprFeed(bucket, name, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// add mutations to the bucket.
+	var vbseqNo = make([]uint64, vbcount)
+	var mutationCount = 1000
+	var mutations = 0
+	addKVset(bucket, mutationCount)
+
+	// observe the mutations from the channel.
+	var e couchbase.UprEvent
+loop:
+	for {
+		select {
+		case e = <-feed.C:
+		case <-time.After(time.Second):
+			break loop
+		}
+		if vbseqNo[e.Vbucket] == 0 {
+			vbseqNo[e.Vbucket] = e.Seqno
+		} else if vbseqNo[e.Vbucket]+1 == e.Seqno {
+			vbseqNo[e.Vbucket] = e.Seqno
+		} else {
+			log.Printf(
+				"sequence number for vbucket %v, is %v, expected %v (%v:%v)\n",
+				e.Vbucket, e.Seqno, vbseqNo[e.Vbucket]+1, string(e.Key),
+				string(e.Value))
+			continue
+		}
+		mutations += 1
+	}
+	feed.Close()
+
+	count := 0
+	for _, seqNo := range vbseqNo {
+		count += int(seqNo)
+	}
+	log.Println("vb sequence:", vbseqNo)
+	if count == mutationCount {
+		log.Printf(
+			"Created %v mutations and observed %v mutations",
+			mutationCount, count)
+	} else {
+		panic(fmt.Errorf("Expected %v mutations got %v", mutationCount, count))
+	}
+}
+
+func getTestConnection(bucketname string) (*couchbase.Bucket, error) {
+	couch, err := couchbase.Connect(TESTURL)
+	if err != nil {
+		log.Println("Make sure that couchbase is at", TESTURL)
+		return nil, err
+	}
+	pool, err := couch.GetPool("default")
+	if err != nil {
+		return nil, err
+	}
+	bucket, err := pool.GetBucket(bucketname)
+	return bucket, err
+}
+
+func addKVset(b *couchbase.Bucket, count int) {
+	for i := 0; i < count; i++ {
+		key := fmt.Sprintf("key%v", i)
+		value := fmt.Sprintf("Hello world%v", i)
+		err := b.Set(key, 0, value)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
