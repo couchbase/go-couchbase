d170b8fe779d6b03d998e5acb7a53bbd44cc71ed
prataprc <prataprc@gmail.com>
2014-10-10 12:44:46 +0530

    Updated upr benchmark program in examples/

--- examples/upr_bench/bench.go
+++ examples/upr_bench/bench.go
@@ -1,88 +1,199 @@
+// Tool receives raw events from go-couchbase UPR client.
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/couchbaselabs/go-couchbase"
 	"log"
+	"os"
+	"runtime/debug"
+	"strings"
 	"time"
+
+	mcd "github.com/couchbase/gomemcached"
+	mc "github.com/couchbase/gomemcached/client"
+	"github.com/couchbase/indexing/secondary/common"
+	"github.com/couchbaselabs/go-couchbase"
 )
 
-var vbcount = 8
+var options struct {
+	buckets    []string // buckets to connect with
+	maxVbno    int      // maximum number of vbuckets
+	stats      int      // periodic timeout(ms) to print stats, 0 will disable
+	printflogs bool
+	info       bool
+	debug      bool
+	trace      bool
+}
 
-const testURL = "http://localhost:9000"
+var done = make(chan bool, 16)
+var rch = make(chan []interface{}, 10000)
 
-// Flush the bucket before trying this program
+func argParse() string {
+	var buckets string
+
+	flag.StringVar(&buckets, "buckets", "default",
+		"buckets to listen")
+	flag.IntVar(&options.maxVbno, "maxvb", 1024,
+		"maximum number of vbuckets")
+	flag.IntVar(&options.stats, "stats", 1000,
+		"periodic timeout in mS, to print statistics, `0` will disable stats")
+	flag.BoolVar(&options.printflogs, "flogs", false,
+		"display failover logs")
+	flag.BoolVar(&options.info, "info", false,
+		"display informational logs")
+	flag.BoolVar(&options.debug, "debug", false,
+		"display debug logs")
+	flag.BoolVar(&options.trace, "trace", false,
+		"display trace logs")
+
+	flag.Parse()
+
+	options.buckets = strings.Split(buckets, ",")
+	if options.debug {
+		common.SetLogLevel(common.LogLevelDebug)
+	} else if options.trace {
+		common.SetLogLevel(common.LogLevelTrace)
+	} else {
+		common.SetLogLevel(common.LogLevelInfo)
+	}
+
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+		os.Exit(1)
+	}
+	return args[0]
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage : %s [OPTIONS] <cluster-addr> \n", os.Args[0])
+	flag.PrintDefaults()
+}
+
 func main() {
-	// get a bucket and mc.Client connection
-	bucket, err := getTestConnection("default")
-	if err != nil {
-		panic(err)
+	cluster := argParse()
+	for _, bucket := range options.buckets {
+		go startBucket(cluster, bucket)
 	}
+	receive()
+}
 
-	// start upr feed
-	name := fmt.Sprintf("%v", time.Now().UnixNano())
-	feed, err := bucket.StartUprFeed(name, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	// add mutations to the bucket.
-	var mutationCount = int64(100000)
-	go addKVset(bucket, int(mutationCount))
-
-	// request stream for a few vbuckets
-	for i := 0; i < 64; i++ {
-		err := feed.UprRequestStream(
-			uint16(i) /*vbno*/, uint32(i) /*opaque*/, 0 /*flag*/, 0, /*vbuuid*/
-			0 /*seqStart*/, 0xFFFFFFFFFFFFFFFF /*seqEnd*/, 0 /*snaps*/, 0)
-		if err != nil {
-			fmt.Printf("%s", err.Error())
+func startBucket(cluster, bucketn string) int {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("%s:\n%s\n", r, debug.Stack())
+			common.StackTrace(string(debug.Stack()))
 		}
+	}()
+
+	common.Infof("Connecting with %q\n", bucketn)
+	b, err := common.ConnectBucket(cluster, "default", bucketn)
+	mf(err, "bucket")
+
+	uprFeed, err := b.StartUprFeed("rawupr", uint32(0))
+	mf(err, "- upr")
+
+	vbnos := listOfVbnos(options.maxVbno)
+
+	flogs, err := b.GetFailoverLogs(vbnos)
+	mf(err, "- upr failoverlogs")
+
+	if options.printflogs {
+		printFlogs(vbnos, flogs)
 	}
 
-	// observe the mutations from the channel.
-	mutations := int64(0)
-	start := time.Now().UnixNano()
-	end := start
+	go startUpr(uprFeed, flogs)
+
 	for {
-		<-feed.C
-		mutations++
-		if mutationCount == mutations {
-			break
+		e, ok := <-uprFeed.C
+		if ok == false {
+			common.Infof("Closing for bucket %q\n", b.Name)
 		}
-		if mutations%(mutationCount/5) == 0 {
-			end = time.Now().UnixNano()
-			log.Printf("Recieved %v mutations %v time lapse per mutation in nano seconds\n",
-				mutations, (end-start)/mutations)
-		}
+		rch <- []interface{}{b.Name, e}
 	}
-
-	log.Printf(" Total time for %d mutations %d ms", mutations, (end-start)/1000000)
-
-	feed.Close()
 }
 
-func getTestConnection(bucketname string) (*couchbase.Bucket, error) {
-	couch, err := couchbase.Connect(testURL)
-	if err != nil {
-		log.Println("Make sure that couchbase is at", testURL)
-		return nil, err
+func startUpr(uprFeed *couchbase.UprFeed, flogs couchbase.FailoverLog) {
+	start, end := uint64(0), uint64(0xFFFFFFFFFFFFFFFF)
+	snapStart, snapEnd := uint64(0), uint64(0)
+	for vbno, flog := range flogs {
+		x := flog[len(flog)-1] // map[uint16][][2]uint64
+		opaque, flags, vbuuid := uint32(vbno), uint32(0), x[0]
+		err := uprFeed.UprRequestStream(
+			vbno, opaque, flags, vbuuid, start, end, snapStart, snapEnd)
+		mf(err, fmt.Sprintf("stream-req for %v failed", vbno))
 	}
-	pool, err := couch.GetPool("default")
-	if err != nil {
-		return nil, err
-	}
-	bucket, err := pool.GetBucket(bucketname)
-	return bucket, err
 }
 
-func addKVset(b *couchbase.Bucket, count int) {
-	for i := 0; i < count; i++ {
-		key := fmt.Sprintf("key%v", i)
-		value := fmt.Sprintf("Hello world%v", i)
-		err := b.Set(key, 0, value)
-		if err != nil {
-			panic(err)
+func mf(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%v: %v", msg, err)
+	}
+}
+
+func receive() {
+	// bucket -> Opcode -> #count
+	counts := make(map[string]map[mcd.CommandCode]int)
+
+	var tick <-chan time.Time
+	if options.stats > 0 {
+		tick = time.Tick(time.Millisecond * time.Duration(options.stats))
+	}
+
+loop:
+	for {
+		select {
+		case msg, ok := <-rch:
+			if ok == false {
+				break loop
+			}
+			bucket, e := msg[0].(string), msg[1].(*mc.UprEvent)
+			if e.Opcode == mcd.UPR_MUTATION {
+				common.Tracef("UprMutation KEY -- %v\n", string(e.Key))
+				common.Tracef("     %v\n", string(e.Value))
+			}
+			if _, ok := counts[bucket]; !ok {
+				counts[bucket] = make(map[mcd.CommandCode]int)
+			}
+			if _, ok := counts[bucket][e.Opcode]; !ok {
+				counts[bucket][e.Opcode] = 0
+			}
+			counts[bucket][e.Opcode]++
+
+		case <-tick:
+			for bucket, m := range counts {
+				common.Infof("%q %s\n", bucket, sprintCounts(m))
+			}
+			common.Infof("\n")
 		}
 	}
+}
+
+func sprintCounts(counts map[mcd.CommandCode]int) string {
+	line := ""
+	for i := 0; i < 256; i++ {
+		opcode := mcd.CommandCode(i)
+		if n, ok := counts[opcode]; ok {
+			line += fmt.Sprintf("%s:%v ", mcd.CommandNames[opcode], n)
+		}
+	}
+	return strings.TrimRight(line, " ")
+}
+
+func listOfVbnos(maxVbno int) []uint16 {
+	// list of vbuckets
+	vbnos := make([]uint16, 0, maxVbno)
+	for i := 0; i < maxVbno; i++ {
+		vbnos = append(vbnos, uint16(i))
+	}
+	return vbnos
+}
+
+func printFlogs(vbnos []uint16, flogs couchbase.FailoverLog) {
+	for i, vbno := range vbnos {
+		common.Infof("Failover log for vbucket %v\n", vbno)
+		common.Infof("   %#v\n", flogs[uint16(i)])
+	}
+	common.Infof("\n")
 }
