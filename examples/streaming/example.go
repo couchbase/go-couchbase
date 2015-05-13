d0265225cfb576f01d690393efa2df2f31688fc6
manik <manik.taneja@gmail.com>
2015-05-13 20:08:37 +0530

    Implement bucket streaming API for getting configuration updates from the couchbase server
    Change-Id: Ifb26fca23581d8e47d73ff73f35e7b74da1faeb1
    Reviewed-on: http://review.couchbase.org/50978
    Reviewed-by: Gerald Sangudi <gerald@couchbase.com>
    Reviewed-by: Colm McHugh <colm@couchbase.com>
    Tested-by: Manik Taneja <manik@couchbase.com>

---
+++ examples/streaming/example.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/couchbase/go-couchbase"
+	"log"
+	"time"
+)
+
+var serverURL = flag.String("serverURL", "http://localhost:9000",
+	"couchbase server URL")
+var poolName = flag.String("poolName", "default",
+	"pool name")
+var bucketName = flag.String("bucketName", "default",
+	"bucket name")
+
+func main() {
+
+	flag.Parse()
+
+	client, err := couchbase.Connect(*serverURL)
+	if err != nil {
+		log.Printf("Connect failed %v", err)
+		return
+	}
+
+	cbpool, err := client.GetPool("default")
+	if err != nil {
+		log.Printf("Failed to connect to default pool %v", err)
+		return
+	}
+
+	var cbbucket *couchbase.Bucket
+	cbbucket, err = cbpool.GetBucket(*bucketName)
+
+	if err != nil {
+		log.Printf("Failed to connect to bucket %v", err)
+		return
+	}
+
+	go performOp(cbbucket)
+
+	errCh := make(chan error)
+
+	cbbucket.RunBucketUpdater(func(bucket string, err error) {
+		log.Printf(" Updated retured err %v", err)
+		errCh <- err
+	})
+
+	<-errCh
+
+}
+
+func performOp(b *couchbase.Bucket) {
+
+	i := 0
+	for {
+		key := fmt.Sprintf("k%d", i)
+		value := fmt.Sprintf("value%d", i)
+		err := b.Set(key, len(value), value)
+		if err != nil {
+			log.Printf("set failed error %v", err)
+			return
+		}
+		i++
+		<-time.After(1 * time.Second)
+
+	}
+}
